25bba1fcfe711fb1242464c253f897913b3c9b1d
Tylam.dev <tylam.dev@gmail.com>
2024-06-18 22:06:43 -0500

    Diccionarios y recorrer

--- main.go
+++ main.go
@@ -3,30 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var array [4]int
-	array[0] = 0
-	array[1] = 1
-	fmt.Println(array[3], len(array), cap(array))
+	m := make(map[string]int)
 
-	//Crear array poblado
-	slice := []int{2, 4, 21, 5, 10, 3, 2, 5}
-	fmt.Println(slice, len(slice), cap(slice))
+	m["Jose"] = 14
+	m["Pepito"] = 20
 
-	//Primer elemento inclusivo, ultimo no inclusivo
-	fmt.Println(slice[0:1])
+	fmt.Println(m)
 
-	// metodo append
-	newSlice := []int{2, 6, 4, 8, 3, 1}
-	newSlice = append(newSlice, 1, 2, 3)
-	slice2 := []int{}
-	// Si no se especifica el tamaño del array es dinamico (slice)
-	var nuevoArray []int
-	nuevoArray = append(nuevoArray, 2, 3)
-	fmt.Println(nuevoArray)
-	slice2 = append(slice2, newSlice...)
-
-	//recorre array
-	for _, i := range slice {
-		fmt.Println(i)
+	//Recorrer map
+	for i, v := range m {
+		fmt.Println(i, v)
 	}
+
+	//Encontrar valor. La variable ok permite verificar el dato
+	value, ok := m["Jose"]
+	fmt.Println(value, ok)
 }
