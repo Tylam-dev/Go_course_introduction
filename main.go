3c8f32212d21c12f9fb752172255b1922294be24
Tylam.dev <tylam.dev@gmail.com>
2024-06-19 19:59:54 -0500

    Ejercicio de manipulacion de punteros

--- main.go
+++ main.go
@@ -10,6 +10,31 @@ type car struct {
 	year  int
 }
 
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+// Creacion de un "metodo" a un struct
+func (myPC pc) ping() {
+	fmt.Println(myPC.brand, "marca")
+}
+
+func (myPC *pc) duplicateRam() {
+	myPC.ram = myPC.ram * 2
+	fmt.Println(myPC.ram)
+}
+
+type noConfigurado struct {
+	Numero   *int
+	Caracter *string
+}
+
+func printPuntero(obj noConfigurado) {
+	fmt.Println(obj.Caracter, obj.Numero)
+}
+
 func main() {
 	myCar := car{brand: "Ford", year: 2020}
 	fmt.Println(myCar)
@@ -30,4 +55,36 @@ func main() {
 
 	//Los metodos y types con mayusculas son Publicos
 	pk.PrintMessage()
+
+	//Punteros
+
+	a := 50
+	b := &a
+
+	fmt.Println(a)
+	fmt.Printf("direccion de memoria %v", b)
+	fmt.Println(*b)
+
+	myPc := pc{ram: 16, disk: 200, brand: "HP"}
+
+	fmt.Println(myPc)
+
+	myPc.ping()
+
+	myPc.duplicateRam()
+
+	prueba := noConfigurado{}
+
+	//Imprime contenido de struct como <nil>
+	printPuntero(prueba)
+
+	//Asignar dato a direccion de memoria del struct
+	numeroAsignar := 21
+	prueba.Numero = &numeroAsignar
+
+	//Imprime el struct con direccion de memoria establecida
+	printPuntero(prueba)
+
+	//Imprime valor de memoria
+	fmt.Println(*prueba.Numero)
 }
